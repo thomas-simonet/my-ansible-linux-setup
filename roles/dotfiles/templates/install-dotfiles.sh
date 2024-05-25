95ba3d680dbb8a2317e5891472bc1ced1ba3fee5
Thomas Simonet <contact@thomassimonet.fr>
2024-05-26 00:01:47 +0200

    fix: nerd fonts / fix: firewall sudo / fix: docker network

--- roles/dotfiles/templates/install-dotfiles.sh
+++ roles/dotfiles/templates/install-dotfiles.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 DOCKER_COMPLETIONS="${HOME}/.cache/zinit/completions/_docker"
+EZA_COMPLETIONS="${HOME}/.cache/zinit/completions/eza"
 
 # Download docker completions, if it's not there
 if [ ! -f "${DOCKER_COMPLETIONS}" ]; then
@@ -8,11 +9,28 @@ if [ ! -f "${DOCKER_COMPLETIONS}" ]; then
   wget -P "$(dirname $DOCKER_COMPLETIONS)" https://raw.githubusercontent.com/docker/cli/master/contrib/completion/zsh/_docker
 fi
 
-echo "Downloading nerd-fonts..."
+# Install nerd fonts
 wget -P ~/.local/share/fonts https://github.com/ryanoasis/nerd-fonts/releases/latest/download/FiraCode.tar.xz \
 && cd ~/.local/share/fonts && tar xf FiraCode.tar.xz && rm FiraCode.tar.xz && fc-cache -fv
 
-echo "Cleaning..."
+# Install eza
+if [ ! -d "/etc/apt/keyrings" ]; then
+  mkdir -p /etc/apt/keyrings
+fi
+
+if [ ! -d "${EZA_COMPLETIONS}" ]; then
+  cd "$(dirname $EZA_COMPLETIONS)" && mkdir -p "eza"
+  cd "$EZA_COMPLETIONS" && git clone https://github.com/eza-community/eza.git .
+fi
+
+wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
+echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
+sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
+sudo apt-get update
+sudo apt-get install -y eza
+# /end eza
+
+# Delete this script
 rm ${HOME}/install-dotfiles.sh
 
 exit 0
